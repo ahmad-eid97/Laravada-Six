5771e4730692485c5c9c3a6347c3ab7c6796b98a
civil.ahmad.eid <civil.ahmad.eid@gmail.com>
2022-12-28 16:43:00 +0200

    add cart pages

--- components/header/AppHeader.vue
+++ components/header/AppHeader.vue
@@ -1,5 +1,17 @@
 <template>
   <header>
+    <div class="cart" :class="openCart ? 'opened' : ''">
+      <div class="head">
+        <i class="fa-regular fa-xmark" @click="openCart = false"></i>
+        <button
+          @click="goToCheckout"
+          :disabled="$store.state.cartItems.length <= 0"
+        >
+          <i class="fa-regular fa-badge-check"></i> Checkout
+        </button>
+      </div>
+      <cart />
+    </div>
     <b-navbar :class="!topOfPage ? 'onScroll' : ''" toggleable="lg">
       <b-navbar-brand :href="localePath('/')">
         <img src="/assets/images/logo.png" alt="logoImage" />
@@ -29,6 +41,10 @@
       </a>
       <a to="contact" class="btn"> Free Qoute </a>
       <lang-switch></lang-switch>
+      <div class="m-0 cartIcon" @click="openCart = !openCart">
+        <span>{{ $store.state.cartItems.length }}</span>
+        <i class="fa-regular fa-cart-plus"></i>
+      </div>
       <div v-if="$store.state.user" class="logout" @click="logout">
         <i class="fa-regular fa-right-from-bracket"></i>
       </div>
@@ -50,12 +66,14 @@
 </template>
 
 <script>
+import cart from "../cart/cart.vue";
 import langSwitch from "../langSwitch/langSwitch.vue";
 // import DropdownMenu from '@innologica/vue-dropdown-menu'
 export default {
   name: "AppHeader",
   components: {
     langSwitch,
+    cart,
     // DropdownMenu
   },
   data() {
@@ -63,6 +81,7 @@ export default {
       show: false,
       show1: false,
       topOfPage: true,
+      openCart: false,
     };
   },
   beforeMount() {
@@ -83,6 +102,10 @@ export default {
         if (!this.topOfPage) this.topOfPage = true;
       }
     },
+    goToCheckout() {
+      this.openCart = false;
+      this.$router.push("/checkout");
+    },
   },
 };
 </script>
@@ -91,6 +114,98 @@ header {
   box-shadow: rgba(0, 0, 0, 0.07) 0px 5px 5px 0px;
   padding: 0;
   padding: 0 20px;
+  .cart {
+    width: 390px;
+    height: 100vh;
+    position: fixed;
+    top: 0;
+    right: 0;
+    transform: translateX(390px);
+    background-color: #fff;
+    z-index: 999999;
+    box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.2);
+    .head {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      padding: 10px;
+      & > i {
+        border: 1px solid var(--main-color);
+        border-radius: 5px;
+        width: 30px;
+        height: 30px;
+        display: grid;
+        place-items: center;
+        cursor: pointer;
+        background-color: var(--main-color);
+        color: #fff;
+        &:hover {
+          color: var(--main-color);
+          background: transparent;
+        }
+      }
+      button {
+        padding: 5px 30px;
+        font-size: 1.1rem;
+        background-color: var(--main-color);
+        color: #fff;
+        border: 1px solid var(--main-color);
+        display: flex;
+        align-items: center;
+        gap: 5px;
+        i {
+          font-size: 1.1rem;
+        }
+        &:disabled {
+          opacity: 0.5;
+          cursor: not-allowed;
+          &:hover {
+            background-color: var(--main-color);
+            color: #fff;
+          }
+        }
+        &:hover {
+          background-color: transparent;
+          color: var(--main-color);
+        }
+      }
+    }
+    &.opened {
+      transform: translateX(0);
+    }
+  }
+  .cartIcon {
+    border: 1px solid var(--main-color);
+    border-radius: 5px;
+    width: 45px;
+    height: 45px;
+    display: grid;
+    place-items: center;
+    cursor: pointer;
+    position: relative;
+    span {
+      position: absolute;
+      top: -15px;
+      right: -10px;
+      width: 30px;
+      height: 30px;
+      background-color: var(--main-color);
+      border-radius: 50%;
+      color: #fff;
+      display: grid;
+      place-content: center;
+      font-size: 1.2rem;
+    }
+    i {
+      color: var(--main-color);
+    }
+    &:hover {
+      background-color: var(--main-color);
+      i {
+        color: #fff;
+      }
+    }
+  }
 }
 .navbar-brand {
   margin: 0 0 0 24px;
